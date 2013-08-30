345d516a318051b31c28be57aaf5b1a7744cc5f9
UzEE <uzee@uzeeinc.net>
2013-08-30 20:52:33 +0500

    Started working on actual routes

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,19 @@
-var test;
+var request = require('request'),
+	winston = require('winston');
+
+module.exports.uploadUserPicture = function(req, res) {
+
+	request.get('http://api.olaround.me/v1/' + req.params.user + '/profile', function(err, result, body) {
+
+		if (err) {
+		
+			winston.error(err);
+			res.send(500, err);
+		
+		} else {
+
+			winston.info(body);
+			res.send(body);
+		}
+	});
+};
