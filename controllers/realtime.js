b4d66f5e3fb92ecd781236b23cff373555dcdbb7
UzEE <uzee@uzeeinc.net>
2013-09-09 14:54:37 +0500

    Begun working on Realtime controller for Facebook

---
+++ controllers/realtime.js
@@ -0,0 +1,10 @@
+var request = require('request'),
+	winston = require('winston'),
+	util = require('util'),
+	azure = require('azure'),
+	fs = require('fs');
+
+module.exports.userUpdated = function(req, res) {
+
+	
+}
