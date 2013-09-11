2993710af0a9b13c74eac596645ad42b7ae15b75
UzEE <uzee@uzeeinc.net>
2013-09-11 11:12:06 +0500

    More progress on the realtime controller

--- controllers/realtime.js
+++ controllers/realtime.js
@@ -2,9 +2,19 @@ var request = require('request'),
 	winston = require('winston'),
 	util = require('util'),
 	azure = require('azure'),
+	ErrorHelper = require('../helpers/error'),
+	UploadHelper = require('../helpers/upload'),
+	mime = require('mime'),
 	fs = require('fs');
 
 module.exports.userUpdated = function(req, res) {
 
-	console.log(util.inspect(req.body, {colors: true, depth: 5}));
+	if (typeof req.body == "undefined" || !req.body || req.body.object != "user" || typeof req.body.entry == "undefined") {
+		ErrorHelper.sendError(req, res, 400);
+	}
+
+	req.body.entry.forEach(function(user, index) {
+
+		
+	});
 }
