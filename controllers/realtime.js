40d7e570d818f0a55646349b5478577094dad8f1
Jahanzaib Aslam <jahanzaibaslam@hotmail.com>
2014-10-01 15:34:53 +0500

    Push

--- controllers/realtime.js
+++ controllers/realtime.js
@@ -204,7 +204,7 @@ module.exports.pushNotification = function(req, res) {
 		winston.info("Pushed notification to GCM");
 	});
 
-	/*hubService.send(tags, data, function(err) {
+	hubService.send(tags, data, function(err) {
 
 		if (err) {
 			winston.error("An error occured while pushing to Template");
@@ -212,7 +212,7 @@ module.exports.pushNotification = function(req, res) {
 		}
 
 		winston.info("Pushed notification to Template");
-	});*/
+	});
 
 	res.send({result: true, status: "pushing"});
 };
