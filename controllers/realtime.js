21f749d89b9ea78e7bda9f8f16c0c3f0285d5e37
Jahanzaib Aslam <jahanzaibaslam@hotmail.com>
2014-10-03 16:44:28 +0500

    call back testing

--- controllers/realtime.js
+++ controllers/realtime.js
@@ -224,6 +224,16 @@ module.exports.pushNotification = function(req, res) {
 	    function (error) {
 	        if (!error) {
 	            // message sent successfully
+	        } else {
+	        	hubService.send(tags, data, function(err) {
+
+					if (err) {
+						winston.error("An error occured while pushing to Template");
+						console.error(util.inspect(err, {colors: true, depth: 7}));
+					}
+
+					winston.info("Pushed notification to Template");
+				});
 	        }
 	    });
 
